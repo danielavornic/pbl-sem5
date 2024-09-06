0ff0d7cf3229fa1ca69b41734aed0c6bbb94d0fe
Daniela Vornic <daniela.vornic@gmail.com>
2024-09-07 01:23:48 +0300

    feat: update color palette and fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   darkMode: ["class"],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+      container: {
+        center: true,
+        padding: "1rem"
+      },
+      fontFamily: {
+        heading: ["var(--font-montserrat)", "var(--font-lato)", "sans-serif"],
+        body: ["var(--font-lato)", "sans-serif"]
       },
       borderRadius: {
         lg: "var(--radius)",
