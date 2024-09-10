d689058ccac6cd21c0e268f6d01401f32d0877b6
maria <118808220+yum3lo@users.noreply.github.com>
2024-09-10 16:03:37 +0300

    feat(register): changed the position of the elements on page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,14 +7,13 @@ import useRegister from "./hooks/use-register";
 const RegisterPage = () => {
   return (
     <PublicLayout title="Creează un cont">
-      <main className="container flex items-start justify-center gap-[10vw]">
+      <main className="container mt-[35px] flex items-start justify-center gap-[8vw]">
         <div className="flex h-[100%] w-[500px] justify-center">
-          <div className="relative w-[290px]">
-            <div className="absolute left-0 top-0 flex h-[150px] w-[150px] items-center justify-center rounded-[30px] bg-muted" />
-            <div className="absolute left-[140px] top-[140px] flex h-[150px] w-[150px] items-center justify-center rounded-[30px] bg-muted" />
-            <div className="absolute left-[140px] top-[300px] flex h-[150px] w-[150px] items-center justify-center rounded-[30px] bg-muted" />
-            <div className="absolute top-[440px] flex h-[150px] w-[150px] items-center justify-center rounded-[30px] bg-muted" />
-            <div className="absolute left-[140px] top-[580px] flex h-[150px] w-[150px] items-center justify-center rounded-[30px] bg-muted" />
+          <div className="relative w-[490px]">
+            <div className="absolute left-0 top-0 flex h-[235px] w-[235px] items-center justify-center rounded-[30px] bg-muted" />
+            <div className="absolute left-[225px] top-[225px] flex h-[235px] w-[235px] items-center justify-center rounded-[30px] bg-muted" />
+            <div className="absolute left-[225px] top-[470px] flex h-[235px] w-[235px] items-center justify-center rounded-[30px] bg-muted" />
+            <div className="absolute top-[695px] flex h-[235px] w-[235px] items-center justify-center rounded-[30px] bg-muted" />
           </div>
         </div>
         <RegisterForm />
