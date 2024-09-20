06d93b059875e56982c1bafd8daeeb3ef5afbff0
maria <118808220+yum3lo@users.noreply.github.com>
2024-09-20 23:49:22 +0300

    feat(organizations): made the page responsive, created the card component

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import OrganizationCard from "@/components/organization-card";
 import PublicLayout from "@/layouts/public";
 
 const OrganizationsPage = () => {
   return (
     <PublicLayout title="Organizații">
-      <main className="container flex items-start justify-center gap-[8vw] py-9">hello world</main>
+      <main className="container">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          <OrganizationCard />
+        </div>
+      </main>
     </PublicLayout>
   );
 };
