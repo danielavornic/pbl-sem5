1371e27d3248a4dd8cc07730197a93009986a24e
maria <118808220+yum3lo@users.noreply.github.com>
2024-09-23 18:28:50 +0300

    feat(organizations): implemented the "search" input and button

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -3,10 +3,15 @@
 import OrganizationCard from "@/components/organization-card";
 import PublicLayout from "@/layouts/public";
 
+import FilteredSearch from "./FilteredSearch";
+
 const OrganizationsPage = () => {
   return (
     <PublicLayout title="Organizații">
       <main className="container">
+        <div className="my-10">
+          <FilteredSearch />
+        </div>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
           <OrganizationCard />
         </div>
