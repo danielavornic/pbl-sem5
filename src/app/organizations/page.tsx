842cd8671b22aa5935f6d3139cb7283bbf443b67
Daniela Vornic <daniela.vornic@gmail.com>
2024-09-23 20:01:26 +0300

    feat(organizations): integrate organizations endpoint

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,20 +1,64 @@
 "use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { useQueryState } from "nuqs";
+import { useMemo } from "react";
+
+import { organizationApi } from "@/api/organizationApi";
 import OrganizationCard from "@/components/organization-card";
+import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
+import { useDebounce } from "@/components/ui/multiple-selector";
 import PublicLayout from "@/layouts/public";
 
-import FilteredSearch from "./FilteredSearch";
-
 const OrganizationsPage = () => {
+  const [searchTerm, setSearchTerm] = useQueryState("search", {
+    defaultValue: ""
+  });
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const organizationQuery = useQuery({
+    queryKey: ["organizations"],
+    queryFn: organizationApi.getAll
+  });
+
+  const filteredOrganizations = useMemo(() => {
+    if (!organizationQuery.data) {
+      return [];
+    }
+
+    return organizationQuery.data.filter((org: any) => {
+      return org.name.toLowerCase().includes(debouncedSearchTerm.toLowerCase());
+    });
+  }, [organizationQuery.data, debouncedSearchTerm]);
+
   return (
     <PublicLayout title="Organizații">
       <main className="container">
-        <div className="my-10">
-          <FilteredSearch />
+        <div className="mb-10 mt-10 flex items-center justify-between">
+          <h1 className="font-heading text-3xl font-bold">Organizații</h1>
+          <div>
+            <Input
+              type="text"
+              placeholder="Caută organizații"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </div>
         </div>
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          <OrganizationCard />
-        </div>
+        {organizationQuery.isLoading ? (
+          <Spinner className="mt-32" />
+        ) : organizationQuery.isSuccess && filteredOrganizations.length > 0 ? (
+          <div className="grid grid-cols-3 gap-6 2xl:grid-cols-4">
+            {filteredOrganizations.map((org: any) => (
+              <OrganizationCard key={org.id} org={org} />
+            ))}
+          </div>
+        ) : (
+          <div className="font-heading font-semibold">
+            Nu s-au găsit organizații care să corespundă criteriilor de căutare.
+          </div>
+        )}
       </main>
     </PublicLayout>
   );
--- src/components/organization-card.tsx
+++ src/components/organization-card.tsx
@@ -9,54 +9,44 @@ import {
   CardHeader,
   CardTitle
 } from "@/components/ui/card";
-import organizations from "@/data/organizations";
+import { Organization } from "@/types";
 
 import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 
-const OrganizationCard = () => {
-  const approvedOrganizations = organizations.filter(
-    (org: any) => org.approvalStatus === "approved"
-  );
-
+const OrganizationCard = ({ org }: { org: Organization }) => {
   return (
-    <>
-      {approvedOrganizations.length > 0 ? (
-        approvedOrganizations.map((org: any) => (
-          <Card key={org.id} className="flex h-full flex-col">
-            <CardHeader className="flex-grow-0 space-y-2">
-              <Badge className="w-fit border-none bg-secondary" variant="outline">
-                <Label className="text-body p-1 text-gray-100">{org.region.name}</Label>
+    <Card key={org.id} className="flex h-full flex-col">
+      <CardHeader className="flex-grow-0 space-y-4">
+        <Badge className="w-fit border-none bg-secondary" variant="outline">
+          <Label className="text-body p-1 text-gray-100">{org.region.name}</Label>
+        </Badge>
+        <img
+          src={org.logo ?? "/images/placeholder.webp"}
+          alt={org.name}
+          className="h-[150px] w-[150px] object-cover"
+        />
+      </CardHeader>
+      <CardContent className="flex flex-grow flex-col justify-between">
+        <div className="flex flex-grow flex-col justify-center space-y-4">
+          <CardTitle>{org.name}</CardTitle>
+          <CardDescription className="line-clamp-3 break-words">{org.description}</CardDescription>
+          <div className="flex flex-wrap gap-2">
+            {org.categories.map((category: any, index: any) => (
+              <Badge key={index} variant="muted">
+                <span className="text-body p-1">{category.name}</span>
               </Badge>
-              <img src={org.logo} alt={org.name} className="h-[150px] w-[150px] rounded-full" />
-            </CardHeader>
-            <CardContent className="flex flex-grow flex-col justify-between">
-              <div className="flex flex-grow flex-col justify-center space-y-4">
-                <CardTitle>{org.name}</CardTitle>
-                <CardDescription className="line-clamp-3 break-words">
-                  {org.description}
-                </CardDescription>
-                <div className="flex flex-wrap gap-2">
-                  {org.categories.map((category: any, index: any) => (
-                    <Badge key={index} variant="muted">
-                      <span className="text-body p-1">{category.name}</span>
-                    </Badge>
-                  ))}
-                </div>
-              </div>
-              <CardFooter className="p-0 pt-4">
-                <Button asChild className="w-full">
-                  <Link href={`/organizations/${org.id}`}>Detalii</Link>
-                </Button>
-              </CardFooter>
-            </CardContent>
-          </Card>
-        ))
-      ) : (
-        <p>Nu există organizații aprobate</p>
-      )}
-    </>
+            ))}
+          </div>
+        </div>
+        <CardFooter className="p-0 pt-4">
+          <Button asChild size="sm" className="ml-auto">
+            <Link href={`/organizations/${org.id}`}>Detalii</Link>
+          </Button>
+        </CardFooter>
+      </CardContent>
+    </Card>
   );
 };
 
