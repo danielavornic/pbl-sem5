de54067a3e20e70789179529787fd9aa64da4624
Daniela Vornic <daniela.vornic@gmail.com>
2024-09-25 01:11:21 +0300

    feat(account): init account page

---
+++ src/app/account/components/sidebar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const menuItems = [
+  {
+    title: "Informații personale",
+    href: "/account"
+  },
+  {
+    title: "Setări",
+    href: "/account/settings"
+  }
+];
+
+export const AccountSidebar = () => {
+  const pathname = usePathname();
+
+  return (
+    <aside className="w-1/4">
+      <nav>
+        <ul>
+          {menuItems.map((item) => (
+            <li key={item.title}>
+              <Button
+                variant="link-foreground"
+                className={cn("px-0", { "font-semibold underline": pathname === item.href })}
+                asChild
+              >
+                <Link href={item.href}>{item.title}</Link>
+              </Button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+};
